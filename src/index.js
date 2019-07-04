1c9f79a41c54767f92e9c2a49b2dee37b9b91344
Edson Machado <edson.machado@grupocmc.es>
2019-07-04 11:23:33 +0200

    Correctly Type-Checking Numbers

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // -----
 
-const immutableNumber = 99;
-let referencedNumber = immutableNumber;
+console.log(typeof 99); // number
 
-referencedNumber = 44;
-
-console.log(immutableNumber); // 99
-console.log(referencedNumber); // 44
+console.log(99 instanceof Number); // false
+console.log(Number('99') instanceof Number); // false
+console.log(new Number('99') instanceof Number); // true
+console.log({}.toString()); // [object Object]
+console.log(Object.prototype.toString.call(99)); // [object Number]
+console.log(Object.prototype.toString.call(99) === '[object Number]'); // true
+console.log(Object.prototype.toString.call(99).slice(8, -1) === 'Number'); // true

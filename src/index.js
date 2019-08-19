ff76cb44288fd393b4a1db32b617fbe962283308
Edson Machado <edson.machado@soprasteria.com>
2019-08-19 09:56:35 +0200

    boolean literal and correctly type-checking

--- src/index.js
+++ src/index.js
@@ -4,27 +4,19 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // -----
+console.log(true, false);
 
-console.log(String.prototype);
+const arg = 'X';
+console.log(Boolean(arg)); // true
+console.log(!!arg); // true
+console.log(Boolean([])); // true
+console.log(Boolean({})); // true
 
-console.log('Pepperoni'.indexOf('P')); // 0
-console.log('Pepperoni'.indexOf('i')); // 8
-console.log('Pepperoni'.indexOf('z')); // -1
+console.log(new Boolean(arg));
 
-console.log('Pepperoni'.includes('P')); // true
-console.log('Pepperoni'.includes('z')); // false
+console.log(typeof true); // boolean
+console.log(typeof false); // boolean
 
-console.log('Pepperoni'.replace('oni', 'fire')); // Pepperfire
-
-console.log('Pepperoni'.slice(2, -1)); // pperon
-console.log('Pepperoni'.slice(0, -1)); // Pepperon
-console.log('Pepperoni'.slice(0, 1)); // P
-console.log('Pepperoni'.slice(0, 'Pepperoni'.length - 1)); // Pepperon
-console.log('Pepperoni'.slice(0, 'Pepperoni'.length)); // Pepperoni
-
-console.log('Pepperoni~Plain'.split('~')); // ["Pepperoni", "Plain"]
-console.log('Pepperoni~Plain'.split('~')[0]); // "Pepperoni"
-console.log('Pepperoni~Plain'.split(/~/)[1]); // "Plain"
-
-console.log('   4444 5555 6666 7777    '.trim()); // 4444 5555 6666 7777
-console.log('   4444 5555 6666 7777    '.replace(/\s/g, '')); // 4444555566667777
+console.log(true instanceof Boolean); // false
+console.log(new Boolean('X') instanceof Boolean); // true
+console.log(Object.prototype.toString.call(true)); // [object Boolean]

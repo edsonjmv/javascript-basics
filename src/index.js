b45c307f8fa65d379df9d41f166f70067fffe9bc
Edson Machado <edson.machado@soprasteria.com>
2019-08-20 14:26:46 +0200

    Rest Parameters and Arguments

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // -----
 
-// name = parameter
-function makeCar(name = 'Porsche') {
-  console.log(`Making car: ${name.toUpperCase()}`);
+// old way
+function makeCarPrice() {
+  console.log(arguments);
+  const total = Array.from(arguments).reduce((prev, next) => prev + next);
+  console.log(`Total: ${total}USD`);
 }
 
-// strings = arguments
-makeCar('Porsche');
-makeCar('Ferrari');
-makeCar();
+makeCarPrice(11, 44, 55, 99, 66);
+
+//new way (rest parameters)
+function makeCarPriceRest(...params) {
+  const total = params.reduce((prev, next) => prev + next);
+  console.log(`Total: ${total}USD`);
+}
+
+makeCarPriceRest(99, 88, 77, 11, 44);

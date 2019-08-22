d1403062a7c26db571d7905ace54ac68f466dc8c
Edson Machado <edson.machado@soprasteria.com>
2019-08-22 11:44:36 +0200

    Immediately-Invoked Function Expressions (IIFE)

--- src/index.js
+++ src/index.js
@@ -5,17 +5,11 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // -----
 
-function makeCarPartID(id) {
+const carPartId = (id => {
   const theId = `CAR_PART_${id}`;
-  return name => `${theId}_${name.toUpperCase()}`;
-}
+  return name => `${theId}_${name}`;
+})('x7H8sFf');
 
-const carPartId = makeCarPartID('x8YdsZ12');
 console.log(carPartId('Left Door'));
 console.log(carPartId('Right Door'));
 console.log(carPartId('Windscreen'));
-
-const anotherCarPartId = makeCarPartID('7hs9zSaq0');
-console.log(anotherCarPartId('Left Door'));
-console.log(anotherCarPartId('Right Door'));
-console.log(anotherCarPartId('Windscreen'));

2943cb7066ca7f6920c5756211aa1f42ef5f6802
Edson Machado <edson.machado@soprasteria.com>
2019-08-26 09:05:21 +0200

    Object Literal, Function and Constructor Syntax

--- src/index.js
+++ src/index.js
@@ -5,20 +5,17 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // -----
 
-const firstCar = { id: 'x8KszK0' };
-const secondCar = { id: 'bc90slqa' };
-const thirdCar = { id: 'h9sNsa' };
+const drink = {
+  id: 'xhs8Pla',
+  name: 'Lemonade',
+  price: 99
+};
 
-function carPartId(name, quantity) {
-  console.log(`${this.id}_${name}_${quantity}`);
-}
+const drinkReference = drink;
+drinkReference.name = 'Peach';
 
-const boundThirdCar = carPartId.bind(thirdCar);
-boundThirdCar('Windscreen', 99);
-boundThirdCar('Exhaust', 9);
+console.log(drink === drinkReference);
 
-// call = c = commas
-carPartId.call(firstCar, 'Left Door', 12);
-
-// apply = a = array
-carPartId.apply(secondCar, ['Right Door', 21]);
+console.log({} instanceof Object);
+console.log(new Object() instanceof Object);
+console.log(Object() instanceof Object);

bbbff3755920acf952ec21669eeb00d30adcfe48
Edson Machado <edson.machado@soprasteria.com>
2019-08-27 12:06:37 +0200

    Correctly Type-Checking Objects

--- src/index.js
+++ src/index.js
@@ -7,16 +7,25 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 const drink = {
   id: 'xhs8Pla',
-  name: 'Lemonade'
+  name: 'Lemonade',
+  price: {
+    sale: 99,
+    full: 129
+  }
 };
 
-const price = {
-  sale: 99,
-  full: 129
+function getType(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
 }
 
-// const mergedDrink = Object.assign({}, drink, { price });
-const mergedDrink = { ...drink, ...{ price } };
+console.log(typeof drink); // object
+console.log(typeof []); // object
+console.log(typeof null); // object
 
-console.log(drink, price);
-console.log(mergedDrink);
+console.log({} instanceof Object); // true
+console.log([] instanceof Object); // true
+console.log(null instanceof Object); // false
+
+console.log(getType(drink)); // object
+console.log(getType(null)); // null
+console.log(getType([])); // array

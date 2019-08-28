872769f0c310fc3474fd55d8c26487d884f500af
Edson Machado <edson.machado@soprasteria.com>
2019-08-28 12:50:14 +0200

    Properties, Indexes and Elements

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // -----
 
 const drinks = ['Lemonade', 'Lime', 'Peach'];
-const drinksReference = drinks;
+
+drinks[0] = 'Diet Lemonade';
+// drinks['favourite'] = 'Cola';
 
 console.log(drinks);
-console.log(drinks === drinksReference); // true
-console.log([] === []); // false
-console.log(new Array('Lemonade', 'Lime', 'Peach'));
-console.log(Array('Lemonade', 'Lime', 'Peach'));
+console.log(drinks[0]);
+console.log(drinks[drinks.length - 1]);
+
+// drinks.length = 0;
+drinks.splice(0, drinks.length);
+
+console.log(drinks);

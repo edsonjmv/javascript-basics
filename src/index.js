19841153f1850c58efdedbed5f39680491854e0e
Edson Machado <edson.machado@soprasteria.com>
2019-08-28 12:07:59 +0200

    Array Literal, Function and Constructor Syntax

--- src/index.js
+++ src/index.js
@@ -5,20 +5,11 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // -----
 
-const drink = {
-  id: 'xhs8Pla',
-  name: 'Lemonade',
-  price: {
-    sale: 99,
-    full: 129
-  }
-};
+const drinks = ['Lemonade', 'Lime', 'Peach'];
+const drinksReference = drinks;
 
-Object.keys(drink).forEach(prop => {
-  console.log(prop);
-  console.log(drink[prop]);
-})
-
-console.log(Object.entries(drink));
-
-console.log(drink);
+console.log(drinks);
+console.log(drinks === drinksReference); // true
+console.log([] === []); // false
+console.log(new Array('Lemonade', 'Lime', 'Peach'));
+console.log(Array('Lemonade', 'Lime', 'Peach'));

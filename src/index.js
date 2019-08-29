c3d15ec71986f23370b5e71fc687e43481552f4c
Edson Machado <edson.machado@soprasteria.com>
2019-08-29 08:50:33 +0200

    Destructuring Arrays

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 const drinks = [['Lemonade', 99], ['Lime', 79], ['Peach', 89]];
 
-const drinkOne = drinks[0];
-const drinkTwo = drinks[1];
+const [ drinkOne, [a, b], ...rest ] = drinks;
 
-console.log(`Drink: ${drinkOne[0]}, Price: ${drinkOne[1]}$`);
-console.log(`Drink: ${drinkTwo[0]}, Price: ${drinkTwo[1]}$`);
-console.log(`Drink: ${drinks[2][0]}, Price: ${drinks[2][1]}$`);
+console.log(drinkOne);
+console.log(a, b);
+console.log(rest);
 
 console.log(drinks);

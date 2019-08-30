3cda5c192ad0487f76bc91093f35e98f23356c94
Edson Machado <edson.machado@soprasteria.com>
2019-08-30 08:39:31 +0200

    Merging Arrays

--- src/index.js
+++ src/index.js
@@ -5,18 +5,12 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // -----
 
-const drinks = [['Lemonade', 99], ['Lime', 79], ['Peach', 89]];
+const drinks = [['Lemonade', 99], ['Lime', 79]];
+const newDrinks = ['Peach', 89];
 
-// Mutable - Shallow cloning
-// const drinksClone = [ ...drinks ];
-// const drinksClone = drinks.slice();
-// const drinksClone = Array.from(drinks);
+// const merged = drinks.concat(newDrinks);
+const merged = [ ...drinks, newDrinks ];
 
-// Immutable - Deep cloning
-const drinksClone = JSON.parse(JSON.stringify(drinks));
-
-drinksClone[0][1] = 1000;
-
-console.log(drinksClone);
-
+console.log(merged);
 console.log(drinks);
+console.log(newDrinks);

ba5412cd1deb6a60070a334363e271ee80a6902c
Edson Machado <edson.machado@soprasteria.com>
2019-08-30 09:35:16 +0200

    Correctly Type-Checking Arrays

--- src/index.js
+++ src/index.js
@@ -5,16 +5,9 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // -----
 
-const drinks = [
-  { name: 'Lemonade', price: 99 },
-  { name: 'Lime', price: 79 },
-  { name: 'Peach', price: 89 },
-]
-
-console.log(drinks.reverse());
-
-// sort lowest to highest
-console.log(drinks.sort((a, b) => a.price - b.price));
-
-// sort highest to lowest
-console.log(drinks.sort((a, b) => b.price - a.price));
+console.log(typeof []); // object
+console.log([] instanceof Array); // true
+console.log(new Array() instanceof Array); // true
+console.log(Array.isArray([])); // true
+console.log(Array.isArray({})); // false
+console.log(Object.prototype.toString.call([])); // [object Array]
